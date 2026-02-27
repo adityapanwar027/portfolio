25776e5ddbfdd23c63f050511819f6dde3aa2463
Aditya Panwar <adityapawar@Adityas-MacBook-Air.local>
2026-02-27 22:33:42 +0530

    Components Created

---
+++ Documents/Portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LandingPage from './Pages/LandingPage'
+function App() {
+  return (
+    <>
+    
+    <LandingPage></LandingPage>
+    
+    </>
+  )
+}
+
+export default App
---
+++ Documents/Portfolio/src/Pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../Components/LandingComponents/Hero'
+function LandingPage() {
+  return (
+   <>
+   <Hero></Hero>
+   </>
+  )
+}
+
+export default LandingPage
