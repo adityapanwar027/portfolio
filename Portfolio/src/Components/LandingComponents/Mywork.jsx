a9e5312ef3c4a72b53642f6e62313b3f03ea23dd
Aditya Panwar <adityapawar@Adityas-MacBook-Air.local>
2026-02-28 15:54:00 +0530

    fix this

--- Portfolio/src/Components/LandingComponents/Mywork.jsx
+++ Portfolio/src/Components/LandingComponents/Mywork.jsx
@@ -3,75 +3,71 @@ import "./Mywork.css";
 
 function Mywork() {
   return (
-    // Top Heading
-    <section class="mywork-root">
-      <div class="mywork-section">
-        <h1 class="mywork-title">
+    <section className="mywork-root">
+      <div className="mywork-section">
+        <h1 className="mywork-title">
           My <span>Work Experience</span>
         </h1>
 
-        <div class="mywork-timeline">
-          {/* <!-- Row 1 --> */}
-          <div class="mywork-row">
-            <div class="mywork-left">
+        <div className="mywork-timeline">
+
+          <div className="mywork-row">
+            <div className="mywork-left">
               <h2>Cognizant, Mumbai</h2>
               <p>Sep 2016 - July 2020</p>
             </div>
 
-            <div class="mywork-circle-wrapper">
-              <div class="mywork-circle-outer">
-                <div class="mywork-circle-inner"></div>
+            <div className="mywork-circle-wrapper">
+              <div className="mywork-circle-outer">
+                <div className="mywork-circle-inner"></div>
               </div>
             </div>
 
-            <div class="mywork-right">
+            <div className="mywork-right">
               <h2>Experience Designer</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                lacus nunc, posuere in justo vulputate, bibendum sodales
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
-          {/* <!-- Row 2 --> */}
-          <div class="mywork-row">
-            <div class="mywork-left">
+          <div className="mywork-row">
+            <div className="mywork-left">
               <h2>Sugee Pvt Limited, Mumbai</h2>
               <p>Sep 2020 - July 2023</p>
             </div>
 
-            <div class="mywork-circle-wrapper">
-              <div class="mywork-circle-outer dark">
-                <div class="mywork-circle-inner"></div>
+            <div className="mywork-circle-wrapper">
+              <div className="mywork-circle-outer dark">
+                <div className="mywork-circle-inner"></div>
               </div>
             </div>
 
-            <div class="mywork-right">
+            <div className="mywork-right">
               <h2>UI/UX Designer</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                lacus nunc, posuere in justo vulputate, bibendum sodales
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
-          {/* <!-- Row 3 --> */}
-          <div class="mywork-row">
-            <div class="mywork-left">
+          <div className="mywork-row">
+            <div className="mywork-left">
               <h2>Cinetstox, Mumbai</h2>
               <p>Sep 2023</p>
             </div>
 
-            <div class="mywork-circle-wrapper">
-              <div class="mywork-circle-outer">
-                <div class="mywork-circle-inner"></div>
+            <div className="mywork-circle-wrapper">
+              <div className="mywork-circle-outer">
+                <div className="mywork-circle-inner"></div>
               </div>
             </div>
 
-            <div class="mywork-right">
+            <div className="mywork-right">
               <h2>Lead UX Designer</h2>
             </div>
           </div>
+
         </div>
       </div>
     </section>
--- Portfolio/src/Pages/LandingPage.jsx
+++ Portfolio/src/Pages/LandingPage.jsx
@@ -3,14 +3,20 @@ import './LandingPage.css'
 import Mywork from '../Components/LandingComponents/Mywork'
 import Hireme from '../Components/LandingComponents/Hireme'
 import Discuss from '../Components/LandingComponents/Discuss'
+import Slider from '../Components/LandingComponents/Slider'
 
 function LandingPage() {
   return (
+
     <div className="landing-layout">
+
       <Mywork />
       <Hireme />
-      <Discuss />
+      <Discuss /> 
+      <Slider></Slider>
+
     </div>
+
   )
 }
 
